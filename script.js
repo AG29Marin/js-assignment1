bfb324e05f861633d81703d84300e9e5f3d8aa5b
Gabriela Marin <90932391+AG29Marin@users.noreply.github.com>
2022-10-14 15:48:50 +0300

    added a user prompt and comments

--- script.js
+++ script.js
@@ -1,49 +1,73 @@
+// Keeping the score
 let player_wins = 0;
 let computer_wins = 0;
 
+// Options into an array
 let options = ["rock", "paper", "scissors"];
 
+// Function for the computer to randomly choose an option.
 function computerPlay() {
     let random = options[Math.floor(Math.random() * options.length)];
     return random;
 }
 
+// Making a loop for playing the game as many times as the user wants
+while (true) {
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection =  prompt("Type Rock/Paper/Scissors: ").toLowerCase();;
-    computerSelection = computerPlay();
-    
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You win!");
-        player_wins += 1;
+    //Prompting the user for his choice of playing the game or not 
+    let play = prompt("Do you want to play a game?(yes/no): ").toLowerCase();
+
+    if (play === "yes") {
+        function playRound(playerSelection, computerSelection) {
+            // Prompting the user for selecting an option
+            playerSelection =  prompt("Type Rock/Paper/Scissors: ").toLowerCase();;
+            computerSelection = computerPlay();
+            
+            // Conditionals for keeping the score
+            if (playerSelection === options[0] && computerSelection === options[2]) {
+                console.log(`The computer selected ${computerSelection}. You win!`);
+                player_wins += 1;
+            }
+
+            else if (playerSelection === options[1] && computerSelection === options[0]) {
+                console.log(`The computer selected ${computerSelection}. You win!`);
+                player_wins += 1;
+            }
+
+            else if (playerSelection === options[2] && computerSelection === options[1]) {
+                console.log(`The computer selected ${computerSelection}. You win!`);
+                player_wins += 1;
+            }
+
+            else if (playerSelection === computerSelection) {
+                console.log(`The computer selected ${computerSelection}. It's a tie!`);
+            }
+
+            else {
+                console.log(`The computer selected ${computerSelection}. You lose!`);
+                computer_wins += 1;
+            }
+            
+        }
+
+        // Function for playing the game five times in a row
+        function game() {
+            for(i = 0; i < 5; i++) {
+                playRound();
+            }
+
+            console.log(`Out of 5 rounds you won: ${player_wins}`);
+
+        }
+
+        // Running the program
+        game();
     }
 
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win!");
-        player_wins += 1;
-    }
-
-    else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You win!");
-        player_wins += 1;
-    }
-
-    else if (playerSelection === computerSelection) {
-        console.log("It's a tie!");
-    }
-    // write a tie
+    // Exiting the program if the user says no
     else {
-        console.log("You lose!");
-        computer_wins += 1;
+        console.log("Goodbye!");
+        break;
+        
     }
-    
 }
-
-function game() {
-    for(i = 0; i < 5; i++) {
-        playRound();
-    }
-    console.log(`Out of 5 rounds you won: ${player_wins}`);
-}
-
-game();
